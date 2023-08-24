fafcd13d80b290df3bde1ca875231c6803980847
Kamil' Gatin <kamilgg26@yandex.ru>
2023-08-25 02:35:56 +0300

    update setup

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name="layerzero-scan",
     version="1.0.0",
-    description="A minimal Python API for LayerZero Scan.",
+    description="A minimal Python API for LayerZero Scan",
     url="https://github.com/kamilgg/layerzero-scan",
-    author="Kamil\' Gatin (kamgg.eth)",
+    author="Kamil Gatin",
     author_email="kamil.gatin2604@gmail.com",
     license="MIT",
     packages=[
@@ -17,4 +17,6 @@ setup(
     install_requires=["requests", "pytest"],
     include_package_data=True,
     zip_safe=False,
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
 )
